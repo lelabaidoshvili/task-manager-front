2a02906bdc0325464fe08754731f2ec5aea79551
lelabaidoshvili <baidoshvililela3@gmail.com>
2023-02-04 19:21:02 +0400

    update auth module

--- src/app/pages/auth/auth.component.ts
+++ src/app/pages/auth/auth.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from "@angular/core";
+import {AuthService} from "../../core/services/auth.service";
+import {BehaviorSubject} from "rxjs";
+import {HttpClient} from "@angular/common/http";
 
 @Component({
-  selector: 'app-auth',
-  templateUrl: './auth.component.html',
-  styleUrls: ['./auth.component.scss']
+  selector: "app-auth",
+  templateUrl: "./auth.component.html",
+  styleUrls: ["./auth.component.scss"]
 })
 export class AuthComponent implements OnInit {
 
-  constructor() { }
+  public loggedIn = false;
+  public isLoggedIn$ = new BehaviorSubject<boolean>(false);
+  constructor(private http: HttpClient, private authService:  AuthService, private cdr: ChangeDetectorRef) {
+    this.isLoggedIn$.next(!!localStorage.getItem)
+  }
 
   ngOnInit(): void {
+    this.isLoggedIn$.subscribe((loggedInStatus) => {
+      this.loggedIn = loggedInStatus;
+      this.cdr.detectChanges();
+    });
   }
+
+
+  public signOut(): void {
+    this.authService.signOut();
+    this.isLoggedIn$.next(false)
+
+  }
+
 
 }
