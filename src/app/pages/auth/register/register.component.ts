0a10e9dfc61f7641285f0a6eff761f5c4a087cea
lelabaidoshvili <baidoshvililela3@gmail.com>
2023-02-04 19:17:00 +0400

    update register component

--- src/app/pages/auth/register/register.component.ts
+++ src/app/pages/auth/register/register.component.ts
@@ -1,14 +1,14 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, NonNullableFormBuilder } from '@angular/forms';
-import { Router } from '@angular/router';
-import { Validators } from '@angular/forms';
-import { AuthService } from '../../../core/services/auth.service';
-import { Register } from 'src/app/core/interfaces';
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, NonNullableFormBuilder } from "@angular/forms";
+import { Router } from "@angular/router";
+import { Validators } from "@angular/forms";
+import { AuthService } from "../../../core/services/auth.service";
+
 
 @Component({
-  selector: 'app-register',
-  templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss'],
+  selector: "app-register",
+  templateUrl: "./register.component.html",
+  styleUrls: ["./register.component.scss"],
 })
 export class RegisterComponent implements OnInit {
   registrationForm: FormGroup;
@@ -18,11 +18,11 @@ export class RegisterComponent implements OnInit {
     private readonly fb: NonNullableFormBuilder
   ) {
     this.registrationForm = fb.group({
-      firstName: ['', [Validators.required]],
-      lastName: ['', [Validators.required]],
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required]],
-      confirmPassword: ['', [Validators.required]],
+      firstName: ["", [Validators.required]],
+      lastName: ["", [Validators.required]],
+      email: ["", [Validators.required, Validators.email]],
+      password: ["", [Validators.required]],
+      confirmPassword: ["", [Validators.required]],
     });
   }
 
@@ -32,35 +32,34 @@ export class RegisterComponent implements OnInit {
 
     this.authService.signUp(this.registrationForm.value).subscribe(() => {
       console.log(this.registrationForm.value);
+      this.router.navigate(["/auth/login"])
     });
   }
-  public onSignIn() {
-    this.router.navigate(['/auth/login']);
-  }
+
 
   ngOnInit() {
-    this.registrationForm.get('password')?.valueChanges.subscribe((pas) => {
-      const confirmPas = this.registrationForm.get('confirmPassword')?.value;
+    this.registrationForm.get("password")?.valueChanges.subscribe((pas) => {
+      const confirmPas = this.registrationForm.get("confirmPassword")?.value;
       if (pas && confirmPas && pas !== confirmPas) {
         this.registrationForm
-          .get('confirmPassword')
+          .get("confirmPassword")
           ?.setErrors({ notMatch: true });
       } else {
-        this.registrationForm.get('confirmPassword')?.setErrors(null);
+        this.registrationForm.get("confirmPassword")?.setErrors(null);
       }
     });
 
     this.registrationForm
-      .get('confirmPassword')
+      .get("confirmPassword")
       ?.valueChanges.subscribe((pas) => {
-        const confirmPas = this.registrationForm.get('password')?.value;
-        if (pas && confirmPas && pas !== confirmPas) {
-          this.registrationForm
-            .get('confirmPassword')
-            ?.setErrors({ notMatch: true });
-        } else {
-          this.registrationForm.get('confirmPassword')?.setErrors(null);
-        }
-      });
+      const confirmPas = this.registrationForm.get("password")?.value;
+      if (pas && confirmPas && pas !== confirmPas) {
+        this.registrationForm
+          .get("confirmPassword")
+          ?.setErrors({ notMatch: true });
+      } else {
+        this.registrationForm.get("confirmPassword")?.setErrors(null);
+      }
+    });
   }
 }
