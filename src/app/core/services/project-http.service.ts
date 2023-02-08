c474acbfdcbc908c3c276b20e4bc25e90f881156
Mariam Lobzhanidze <mariami.lobzhanidze7@gmail.com>
2023-02-08 18:40:01 +0400

    resolved

--- src/app/core/services/project-http.service.ts
+++ src/app/core/services/project-http.service.ts
@@ -2,13 +2,13 @@ import { Injectable } from '@angular/core';
 
 import { Project } from '../interfaces';
 import { BaseService } from './base.service';
-import {Observable} from "rxjs";
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProjectHttpService extends BaseService {
-  createProject(payload: Project):Observable<Project> {
+  createProject(payload: Project): Observable<Project> {
     return this.post<Project>('project', payload);
   }
 }
