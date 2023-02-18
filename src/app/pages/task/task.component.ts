796c3d8bd0ba4e186d437eea3d36798958518cc0
Mariam Lobzhanidze <mariami.lobzhanidze7@gmail.com>
2023-02-18 23:15:22 +0400

    changes for merge

--- src/app/pages/task/task.component.ts
+++ src/app/pages/task/task.component.ts
@@ -23,92 +23,19 @@ import { Router } from '@angular/router';
   styleUrls: ['./task.component.scss'],
 })
 export class TaskComponent implements OnInit {
-  @ViewChild('sidenav') sidenav: MatSidenav;
-
-  reason = '';
-
-  close(reason: string) {
-    this.reason = reason;
-    this.sidenav.close();
-  }
-
-  shouldRun = /(^|.)(stackblitz|webcontainer).(io|com)$/.test(
-    window.location.host
-  );
-
-  drop(event: CdkDragDrop<string[]>) {
-    if (event.previousContainer === event.container) {
-      moveItemInArray(
-        event.container.data,
-        event.previousIndex,
-        event.currentIndex
-      );
-    } else {
-      transferArrayItem(
-        event.previousContainer.data,
-        event.container.data,
-        event.previousIndex,
-        event.currentIndex
-      );
-    }
-  }
-
-  //---
-
-  currentProject: Project;
-
-  //--
-  myProjects: Project[] = [];
-  task = '';
-  myBoard: BoardResponse[] = [];
-  myIssue: IssueTypeResponse[] = [];
-  usersData: UsersDataResponse[] = [];
-
-  constructor(
-    private projectFacadeService: ProjectFacadeService,
-    private boardFacadeService: BoardFacadeService,
-    private IssueTypeFacadeService: IssueTypeFacadeService,
-    private userFacadeService: UsersFacadeService,
-    private stepperService: StepperService,
-    private router: Router
-  ) {}
-
-  ngOnInit(): void {
-    //------
-    this.projectFacadeService.current$.subscribe((res) => {
-      this.currentProject = res;
-
-      console.log(res);
-
-      console.log(this.currentProject);
-    });
-    //-----------
-
-    this.IssueTypeFacadeService.getIssueTypes().subscribe((Issue) => {
-      console.log(Issue);
-      this.myIssue = Issue;
-    });
-    this.boardFacadeService.getBoards().subscribe((Board) => {
-      console.log(Board);
-      this.myBoard = Board;
-    });
-    this.projectFacadeService
-      .getMyProjects()
-      .pipe(
-        map((projects) => {
-          if (projects.length > 0) {
-            this.myProjects = projects;
-
-            console.log('my projects');
-            console.log(projects);
-          }
-        })
-      )
-      .subscribe((projects) => {});
-  }
-
-  openBoardForm() {
-    this.router.navigate(['/stepper']);
-    this.stepperService.goToStep(1);
-  }
+  ngOnInit(): void {}
 }
+
+//  //------
+//  this.projectFacadeService.current$.subscribe((res) => {
+//   this.currentProject = res;
+
+//   console.log(res);
+
+//   console.log(this.currentProject);
+// });
+// //-----------
+// openBoardForm() {
+//   this.router.navigate(['/stepper']);
+//   this.stepperService.goToStep(1);
+// }
