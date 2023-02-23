45ab0dd24959eef9fd89a1a51f4806128fae25b7
Mariam Lobzhanidze <mariami.lobzhanidze7@gmail.com>
2023-02-23 14:32:17 +0400

    changes saved

--- src/app/pages/task/add-task/add-task.component.ts
+++ src/app/pages/task/add-task/add-task.component.ts
@@ -10,48 +10,7 @@ import { TasksFacadeService } from 'src/app/facades/tasks-facade.sevice';
   styleUrls: ['./add-task.component.scss'],
 })
 export class AddTaskComponent implements OnInit {
-  // taskFormGroup: FormGroup;
-  // taskPropertyGroup: FormGroup;
+  constructor(private taskFacadeService: TasksFacadeService) {}
 
-  // priority = TaskPriority;
-  // priorityEnum = [];
-  // tasks = TaskStatus;
-  // taskEnum = [];
-
-  // setCheckBox: boolean = false;
-  constructor(private taskFacadeService: TasksFacadeService) {
-    // this.priorityEnum = Object.keys(this.priority);
-    // this.taskEnum = Object.keys(this.tasks);
-  }
-
-  ngOnInit(): void {
-    // this.taskPropertyGroup = new FormGroup({
-    //   name: new FormControl(null, Validators.required),
-    //   filedName: new FormControl(null, Validators.required),
-    //   value: new FormControl(null, Validators.required),
-    //   isRequired: new FormControl(false, Validators.required),
-    // });
-    // this.taskFormGroup = new FormGroup({
-    //   name: new FormControl(null, Validators.required),
-    //   description: new FormControl(null, Validators.required),
-    //   issueTypeId: new FormControl(null, Validators.required),
-    //   taskProperty: new FormArray([]),
-    //   // epicId: new FormControl(null, Validators.required),
-    //   boardId: new FormControl(null, Validators.required),
-    //   boardColumnId: new FormControl(null, Validators.required),
-    //   isBacklog: new FormControl(false, Validators.required),
-    //   priority: new FormControl(null, Validators.required),
-    //   taskStatus: new FormControl(null, Validators.required),
-    //   assigneeId: new FormControl(null, Validators.required),
-    //   reporterId: new FormControl(null, Validators.required),
-    // });
-  }
-
-  // submit() {
-  //   this.taskFacadeService
-  //     .createTask(this.taskFormGroup.value)
-  //     .subscribe((res) => {
-  //       console.log(res);
-  //     });
-  // }
+  ngOnInit(): void {}
 }
