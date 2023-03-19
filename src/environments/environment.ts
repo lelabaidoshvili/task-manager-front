7b7cfb8d3a738db84f089507c385a86d498801dc
lelabaidoshvili <baidoshvililela3@gmail.com>
2023-03-19 17:56:33 +0400

    update enviroments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   apiUrl: 'https://task-manager-1-production.up.railway.app/api/',
 };
 
